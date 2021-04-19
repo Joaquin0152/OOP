9d090e32811f05aaf34873e2c336fed45f33d44f
Joaquin Maravillo <joaquin297545@gmail.com>
2021-04-18 21:28:18 -0500

    adding questions to the intern

--- index.js
+++ index.js
@@ -3,54 +3,85 @@ const manager = require('./data/manager')
 const intern = require('./data/intern')
 const engineer = require('./data/engineer')
 const fs = require('fs');
-const {
-    writeFile
-} = require('node:fs');
 const myteam = require('./public')
 
 myTeam[];
-const promptUser = () => {
-return inquirer.prompt([{
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            name: 'ID',
-            message: 'What is your ID?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?',
-        },
-        {
-            type: 'list',
-            name: 'role',
-            message: 'What is your profession?',
-            choices: ['Employee', 'Manager', 'Engineer', 'Intern']
-        },
-    ])
-    .then((managerQues) => {
-        const manager = bossMan(manager.id, manager.name, manager.email, manager.officeNumber)
-        team.push(manger)
-        switch (manager.addMember) {
-            case 'Engineer':
-                engineerQues() {
-                    break;
-                    default:
-                    writeFile('public/team.html', generateTeam(team))
-                }
-    });
+const bossMan = () => {
+        return inquirer.prompt([{
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is your name?',
+                },
+                {
+                    type: 'input',
+                    name: 'id',
+                    message: 'What is your ID?',
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'What is your email?',
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is your profession?',
+                    choices: ['Employee', 'Manager', 'Engineer', 'Intern']
+                },
+                {
+                    type: 'list',
+                    name: 'officeNumber',
+                    message: 'What is your office number?',
+                    choices: ['23', '7', '25', '330']
+                },
+            ])
+            .then((managerQues) => {
+                    const manager = bossMan(manager.id, manager.name, manager.email, manager.officeNumber)
+                    team.push(manager)
+                    switch (manager.addMember) {
+                        case 'Engineer':
+                            engineerQues() {
+                                break;
+                                default:
+                                writeFile('public/team.html', generateTeam(team))
+                            }
+                    });
+            }
 
-{
-    type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub Username',
-}, {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address',
-},
-]);
+        const internQuest = () => {
+                return inquirer.prompt([{
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is your name?',
+                }, {
+                    type: 'input',
+                    name: 'id',
+                    message: 'What is your ID?',
+                }, {
+                    type: 'input',
+                    name: 'email',
+                    message: 'What is your email?',
+                }, {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is your profession?',
+                    choices: ['Employee', 'Manager', 'Engineer', 'Intern']
+                }, {
+                    type: 'list',
+                    name: 'school',
+                    message: 'What college do you attend?',
+                    choices: ['SMU', 'A&M', 'UTD', 'TWU']
+                }, 
+            ])
+            .then((internQues) => {
+                    const intern = bossMan(intern.id, intern.name, intern.email, intern.school)
+                    team.push(intern)
+                    switch (intern.addMember) {
+                        case 'Engineer':
+                            engineerQues() {
+                                break;
+                                default:
+                                writeFile('public/team.html', generateTeam(team))
+                            }
+                    });
+            }
